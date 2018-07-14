41e511eae772a40318b740534a87cb31eda65c9e
Zhang Kang <kang.zhang@emc.com>
2018-07-14 22:43:42 +0800

    add pool

---
+++ pool.go
@@ -0,0 +1,25 @@
+// Package gpool implements a tcp connection pool to manage connection and use
+package gpool
+
+import (
+	"errors"
+	"net"
+)
+
+var (
+	ErrClosed = errors.New("pool has been closed")
+)
+
+type Pool interface {
+	// Get returns a new connection from pool.
+	Get() (net.Conn, error)
+
+	// Close close the pool and reclaim all the connections.
+	Close()
+
+	// Len get the length of the pool
+	Len()
+
+	// Idle get the idle connection pool number
+	Idle()
+}
