d579b57c78ad39c39a702ecfd1edfc5c3cdac14d
broadroad <tjbroadroad@163.com>
2018-07-21 10:11:02 +0800

    fix example bug

--- example/example.go
+++ example/example.go
@@ -3,21 +3,23 @@ package main
 import (
 	"fmt"
 	"net"
+
+	"github.com/broadroad/gpool"
 )
 
 func main() {
 	// factory is the function that create connection
-	factory = func() (net.Conn, error) { return net.Dial(network, address) }
+	factory := func() (net.Conn, error) { return net.Dial("tcp", "127.0.0.1") }
 
 	// poolConfig is the config of gpool
-	poolConfig = &PoolConfig{
+	poolConfig := &PoolConfig{
 		InitCap: 5,
 		MaxCap:  30,
 		Factory: factory,
 	}
 
 	// create a new conn pool
-	p, err := NewGPool(poolConfig)
+	p, err := gpool.NewGPool(poolConfig)
 	if err != nil {
 		fmt.Println("new pool error is", err)
 	}
