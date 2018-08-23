845295d75a80923ca65b701ed3eecff03d691c4b
ctyin <ctyin@freewheel.tv>
2018-08-24 00:34:55 +0800

    modify example

--- example/example.go
+++ example/example.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
+	"time"
 
 	"github.com/Broadroad/gpool"
 )
 
-func main() {
-	// factory is the function that create connection
-	factory := func() (net.Conn, error) { return net.Dial("tcp", "127.0.0.1") }
-
-	// poolConfig is the config of gpool
-	poolConfig := &gpool.PoolConfig{
-		InitCap: 5,
-		MaxCap:  30,
-		Factory: factory,
+var (
+	network    = "tcp"
+	address    = "127.0.0.1:8080"
+	factory    = func() (net.Conn, error) { return net.Dial(network, address) }
+	poolConfig = &gpool.PoolConfig{
+		InitCap:     5,
+		MaxCap:      30,
+		Factory:     factory,
+		IdleTimeout: 15 * time.Second,
 	}
+)
+
+func main() {
 
 	// create a new conn pool
 	p, err := gpool.NewGPool(poolConfig)
@@ -27,21 +32,66 @@ func main() {
 	// release pool
 	defer p.Close()
 
-	// get a new connection from pool
-	conn, err := p.Get()
-	if err != nil {
-		fmt.Println("Get error:", err)
+	done := make(chan struct{})
+
+	for i := 0; i < 20; i++ {
+		go func() {
+
+			defer func() {
+				done <- struct{}{}
+			}()
+
+			// get a new connection from pool
+			conn, err := p.Get()
+			if err != nil {
+				fmt.Println("Get error:", err)
+			}
+
+			_, ok := conn.(*gpool.GConn)
+			if !ok {
+				fmt.Println("Conn is not of type GConn")
+			} else {
+				time.Sleep(time.Second * 1)
+				conn.Close()
+			}
+		}()
 	}
 
-	_, ok := conn.(*gpool.GConn)
-	if !ok {
-		fmt.Println("Conn is not of type GConn")
+	for i := 0; i < 20; i++ {
+		<-done
 	}
-
-	// return connection to pool
-	conn.Close()
 
 	// return len of the pool
-	fmt.Println("len=", p.Len())
+	fmt.Println("len = ", p.Len())
 
+}
+
+func tcpServer() {
+	l, err := net.Listen(network, address)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Panicln(err)
+		}
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		size, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
+		data := buf[:size]
+		conn.Write(data)
+	}
 }
